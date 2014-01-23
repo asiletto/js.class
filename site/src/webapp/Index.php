9db4d3efa71046b9257f65fdd5b57d4a32051cc0
asiletto <alessandro.siletto@gmail.com>
2014-01-23 11:24:19 +0000

    bootstrap base template

---
+++ site/src/webapp/Index.php
@@ -0,0 +1,25 @@
+<?php
+
+use Tonic\UnauthorizedException;
+use Tonic\Response;
+/**
+ * @uri /
+ * 
+ */
+class Index extends BaseResource {
+
+	/**
+	 * @method GET
+	 */
+	function hello() {
+		$smarty = $this->container['smarty'];
+		$log = $this->container['log'];
+		$log->addDebug($this->getLoggedUsername().' executing '.get_class($this).'.'.__FUNCTION__.'() '.$_SERVER['REQUEST_URI'], array("GET"=>$_GET, "POST"=>$_POST));
+		
+		
+		return $smarty->fetch('index.html');
+	}
+	
+}
+
+?>
