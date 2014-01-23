6f97b49e784f32b3803b8f134617390318cb797f
asiletto <alessandro.siletto@gmail.com>
2014-01-23 17:21:55 +0000

    node hello world examples

---
+++ node/hello-func-http.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+function handleRequest(request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello world!");
+  response.end();
+}
+
+http.createServer(handleRequest).listen(8080);
---
+++ node/hello-func.js
@@ -0,0 +1,9 @@
+function say(something){
+ console.log(something);
+}
+
+function exec(funct, val){
+ funct(val);
+}
+
+exec(say, "Hello world!");
